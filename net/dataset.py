f3b347cf7528c18912c0f1e8c9116e18e1350331
Allen Zhang <cz2119@log-1.nyu.cluster>
2021-12-04 21:21:52 -0500

    added augmentations

--- net/dataset.py
+++ net/dataset.py
@@ -1,4 +1,5 @@
 from torchvision import datasets, transforms
+import torch
 
 def get_dataset(dataset, aug):
     avail_datasets = {
@@ -20,12 +21,52 @@ def get_cifar10(aug=False):
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
     ])
+    ### list of augmentations
+    aug_transform_list = []
+    aug_transform_list.append(transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+    ]))
+    aug_transform_list.append(transforms.Compose([
+        transforms.ColorJitter(brightness=5, contrast=5, saturation=5, hue=0.4),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+    ]))
+    aug_transform_list.append(transforms.Compose([
+        transforms.RandomAffine(degrees =15, translate=(0.1,0.1), shear=2),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+    ]))
+    aug_transform_list.append(transforms.Compose([
+        transforms.RandomPerspective(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+    ]))
+    aug_transform_list.append(transforms.Compose([
+        transforms.GaussianBlur(5, sigma=(0.1, 2.0)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+    ]))
+    ### concat trainset
+    trainset = []
+    for augments in aug_transform_list:
+        trainset.append(datasets.CIFAR10(
+        root='./', 
+        train=True, 
+        download=True, 
+        transform=(augments if aug else ori_transform)))
+    trainset = torch.utils.data.ConcatDataset(trainset)
+
+    '''
     trainset = datasets.CIFAR10(
         root='./', 
         train=True, 
         download=True, 
         transform=(aug_transform if aug else ori_transform)
     )
+    '''
     testset = datasets.CIFAR10(
         root='./', 
         train=False, 
